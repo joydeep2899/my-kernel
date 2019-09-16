25c6c7f49c7dcbc36d6089568da80fde95c6a5b2
joydeep2899 <joydeep779@gmail.com>
2019-09-17 03:28:05 +0530

    few bug fixes

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 echo "compiled kernel.c "
  gcc -m32 -c kernel.c -o kc.o -fno-stack-protector
  echo "compiled asembly file "
- nasm kernel.asm -o kasm.o -f elf
+ nasm kernel.asm -o kasm.o -f elf 
  
- echo "linked the files "
- ld -m elf_i386 -T link.ld -O kernel kasm.o kc.o
+ echo "linking  the files "
+ 
+ ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 
 
 echo "running the emulator "
